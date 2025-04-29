b417c55e8a0971f455ef146028bc07379bd4a7e6
Francisco <francisco.lopez.ponce@ciencias.unam.mx>
2025-04-28 19:24:55 -0600

    class update

--- Kurosagol/Kurosagol.py
+++ Kurosagol/Kurosagol.py
@@ -1,18 +1,28 @@
-class Ejemplo:
-	def __init__(
-		self,
-		model_id
-	):
-		self.model_id = model_id
-		self.params = ['hola', 'prueba', 'dinosaurios']
+#class Ejemplo:
+#	def __init__(
+#		self,
+#		model_id
+#	):
+#		self.model_id = model_id
+#		self.params = ['hola', 'prueba', 'dinosaurios']
+#
+#	def print_params(self):
+#		for _ in self.params:
+#			print(_)
 
-	def print_params(self):
-		for _ in self.params:
-			print(_)
 
+class DatasetLoader:
+	def __init__(self, dataset):
+		self.dataset = dataset
 
-def test_function(dataset):
-	return dataset['chosen']
+	def test_function(self, dataset):
+		return dataset['chosen']
 
-def length(dataset):
-	return len(dataset)
+	def test_length(self, dataset):
+		return len(dataset)
+
+#def test_function(dataset):
+#	return dataset['chosen']
+
+#def length(dataset):
+#	return len(dataset)
