12ea92333de566dda2d5311e982275b482b45dd8
Francisco <francisco.lopez.ponce@ciencias.unam.mx>
2025-08-19 18:23:43 -0600

    test.py pt 10

--- Kurosagol/test.py
+++ Kurosagol/test.py
@@ -2,4 +2,4 @@ from transformers import pipeline
 
 model_name_or_path = 'Kurosawama/gemma-3-1b-it-Full-align'
 pipe = pipeline("text-generation", model=model_name_or_path)
-print(pipe("This movie was really")[0]["generated_text"])
+print(pipe("Translate the following set of premises to First-Order Logic: All men are mortal. Plato is a man.")[0]["generated_text"])
