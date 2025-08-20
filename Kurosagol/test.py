d36cb342f4ca7859ab40063c9b8a0de636c3cb5c
Francisco <francisco.lopez.ponce@ciencias.unam.mx>
2025-08-19 18:12:02 -0600

    .

--- Kurosagol/test.py
+++ Kurosagol/test.py
@@ -5,7 +5,7 @@ import torch
 dev = ('cuda' if torch.cuda.is_available() else 'cpu')
 
 base_model = 'Kurosawama/Llama-3.1-8B-Full-align'
-adapter_model_name = 'media/discoexterno/francisco/modelos/checkpoint-1128'
+adapter_model_name = 'media/discoexterno/francisco/modelos/checkpoint-1128\adapter_model.safetensors'
 
 tokenizer = AutoTokenizer.from_pretrained(base_model)
 model = AutoModelForCausalLM.from_pretrained(base_model)
